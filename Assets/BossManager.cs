cb2cd9a42acdeb5dbb87259122c7b3134802cba2
- <->
2026-02-27 21:15:29 -0800

    Render sprite.

---
+++ Assets/BossManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    public SpriteRenderer HEAD_RENDERER;
+    public SpriteRenderer TORSO_RENDERER;
+    public SpriteRenderer LEGS_RENDERER;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
